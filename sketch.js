0717d63681a54ae1c56101b0cd19a4d770eb73f5
Laxmi <laxmidugginenei1995@gmail.com>
2020-09-08 02:49:48 +0530

    angry birds edits

--- sketch.js
+++ sketch.js
@@ -17,6 +17,9 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
+
+    platform = new Ground(150, 300, 300, 170);
+
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
@@ -61,4 +64,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    
 }
